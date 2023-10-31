2a59b3b860733693ea4fe7c01dff090bd139c66a
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-10-31 17:43:17 +0100

    Create app

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function main() {
+
+  // create new window
+  let mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    width: 520,
+    height: 650,
+    frame: false,
+  })
+
+  // load app/index.html as the window content
+  mainWindow.loadFile(path.join('app', 'index.html'));
+
+  // add developer
+  //mainWindow.webContents.openDevTools();
+}
+
+app.on('ready', main);
+
+app.on('window-all-closed', function () {
+  app.quit();
+});
